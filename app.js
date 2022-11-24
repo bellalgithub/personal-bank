c50562905cadc472731a5d34ebad91318fdf8635
bellal <bellal.sq@gmail.com>
2022-11-24 10:15:27 +0600

    Deposit option updated

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 
 
+
+// Login button Event Handeler
+
 const loginBtn = document.getElementById('loginBtn');
 loginBtn.addEventListener('click', function () {
+
 
     const loginArea = document.getElementById('login-section');
     loginArea.style.display = 'none';
@@ -9,4 +13,21 @@ loginBtn.addEventListener('click', function () {
     transactionArea.style.display = 'block';
 
 })
-m
+
+// Deposit button event handler
+
+const depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener('click', function () {
+
+    const depositAmount = document.getElementById('depositAmount').value;
+    const depositNumber = parseFloat(depositAmount);
+    const currentDeposit = document.getElementById('currentAmount').innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+
+    const totalDeposit = depositNumber + currentDepositNumber;
+    document.getElementById('currentAmount').innerText = totalDeposit;
+    document.getElementById('depositAmount').value = ''
+
+})
+
+
